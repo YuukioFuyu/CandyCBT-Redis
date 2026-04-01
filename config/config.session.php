8c6f9ddff28af47e7f004d17c4abf75cc8b6f764
Yuukio Fuyu <yuukio.fuyu@aol.com>
2026-04-02 04:47:12 +0700

    Refactor Redis auth string encoding

--- config/config.session.php
+++ config/config.session.php
@@ -5,17 +5,16 @@ $redisHost = isset($_ENV['REDIS_HOST']) ? $_ENV['REDIS_HOST'] : '127.0.0.1';
 $redisPort = isset($_ENV['REDIS_PORT']) ? $_ENV['REDIS_PORT'] : 6379;
 $redisUser = isset($_ENV['REDIS_USERNAME']) ? $_ENV['REDIS_USERNAME'] : '';
 $redisPass = isset($_ENV['REDIS_PASSWORD']) ? $_ENV['REDIS_PASSWORD'] : '';
-$redisPrefix = isset($_ENV['REDIS_PREFIX']) ? $_ENV['REDIS_PREFIX'] . 'PHPREDIS_SESSION:' : 'PHPREDIS_SESSION:';
-$redisDb = isset($_ENV['REDIS_DATABASE']) ? $_ENV['REDIS_DATABASE'] : 0;
-$redisTimeout = isset($_ENV['REDIS_TIMEOUT']) ? $_ENV['REDIS_TIMEOUT'] : 0;
+$redisPrefix = urlencode(isset($_ENV['REDIS_PREFIX']) ? $_ENV['REDIS_PREFIX'] . 'sessions:' : 'sessions:');
 
 $authString = "";
 if (!empty($redisUser) && !empty($redisPass)) {
-    $authString = "&auth=[" . $redisUser . "," . $redisPass . "]";
+    $authString = "&auth%5B0%5D=" . urlencode($redisUser) . "&auth%5B1%5D=" . urlencode($redisPass);
 } elseif (!empty($redisPass)) {
-    $authString = "&auth=" . $redisPass;
+    $authString = "&auth=" . urlencode($redisPass);
 }
-$redisSavePath = "tcp://{$redisHost}:{$redisPort}?prefix={$redisPrefix}&timeout={$redisTimeout}&database={$redisDb}{$authString}";
+
+$redisSavePath = "tcp://{$redisHost}:{$redisPort}?prefix={$redisPrefix}{$authString}";
 
 ini_set('session.save_handler', 'redis');
 ini_set('session.save_path', $redisSavePath);
